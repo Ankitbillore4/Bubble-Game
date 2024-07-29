e238caf5085478ae4d7511c1dcf342a28cba47a0
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-29 23:22:00 +0530

    js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
 
-var clutter = ""
-for(var i = 1 ; i<=160 ; i++ ){
+function Bubble() {
+    var clutter = ""
+    for (var i = 1; i <= 160; i++) {
 
-var rn= Math.floor(Math.random()*10 )
-    clutter +=` <div id="bubble">${rn}</div>`
+        var rn = Math.floor(Math.random() * 10)
+        clutter += ` <div id="bubble">${rn}</div>`
+    }
+    document.querySelector("#pcenter").innerHTML = clutter
 }
-document.querySelector("#pcenter").innerHTML=clutter
+
+var Timmer = 60;
+function runTimmer(){
+setInterval(function(){
+ Timmer--
+ document.querySelector("#timmer").textContent=Timmer
+},1000)
+}
+runTimmer()
+
+
+Bubble()
